7ae9ea83d3ab155a05e356172a284e16946551a2
RCmerci <562837353@qq.com>
2014-08-18 17:04:40 +0800

    快到一半了= =

--- implement_stdlib_list.erl
+++ implement_stdlib_list.erl
@@ -10,7 +10,23 @@
          duplicate/2,
          filter/2,
          filtermap/2,
-         flatlength/1]).
+         flatlength/1,
+         flatmap/2,
+         flatten/1,
+         flatten/2,
+         foldl/3,
+         foldr/3,
+         foreach/2,
+         keydelete/3,
+         keyfind/3,
+         keymap/3,
+         keymember/3,
+         keymerge/3,
+         keyreplace/4,
+         keysearch/3,
+         keysort/2,
+         keystore/4,
+         keytake/3]).
 -export([get_reverse_list/2]).
 
 all(_, []) -> true;
@@ -121,3 +137,157 @@ filtermap(Fun, [First|Rest]) ->
 flatlength([]) -> 0;
 flatlength([First|Rest]) when is_list(First) -> flatlength(First) + flatlength(Rest);
 flatlength([_|Rest]) -> 1 + flatlength(Rest).
+
+
+flatmap(_, []) -> [];
+flatmap(Fun, [First|Rest]) ->
+    append(Fun(First), flatmap(Fun, Rest)).
+
+flatten([]) -> [];
+flatten([First|Rest]) when is_list(First)->
+    append(flatten(First), flatten(Rest));
+flatten([First|Rest]) ->
+    [First | flatten(Rest)].
+
+flatten(DeepList, Tail) ->
+    append(flatten(DeepList), Tail).
+
+
+foldl(_, Acc, []) -> Acc;
+foldl(Fun, Acc0, [First|Rest]) -> foldl(Fun, Fun(First, Acc0), Rest).
+
+
+foldr(Fun, Acc0, List) ->
+    foldl(Fun, Acc0, reverse(List)).
+
+foreach(_, []) -> ok;
+foreach(Fun, [First|Rest]) ->
+    Fun(First),
+    foreach(Fun, Rest).
+
+
+keydelete(_Key, _N, []) -> [];
+keydelete(Key, N, [First|Rest]) when is_tuple(First), N>=1->
+    try element(N, First) == Key of
+        true ->
+            Rest;
+        false ->
+            [First | keydelete(Key, N, Rest)]
+    catch
+        error:badarg ->
+            [First | keydelete(Key, N, Rest)]
+    end.
+
+
+keyfind(_Key, _N, []) -> false;
+keyfind(Key, N, [First|Rest]) when is_tuple(First), N>=1 ->
+    try element(N, First) == Key of
+        true ->
+            First;
+        false ->
+            keyfind(Key, N, Rest)
+    catch
+        error:badarg ->
+            keyfind(Key, N, Rest)
+    end.
+
+
+keymap(_Fun, _N, []) ->[];
+keymap(Fun, N ,[First|Rest]) when is_tuple(First), N>=1 ->
+    L = length(tuple_to_list(First)),
+    Func = fun(K, Acc0) ->
+                   Temp = fun(This, I, Acc) ->
+                                  case I of
+                                      N ->
+                                          This(This, I+1, erlang:append_element(Acc, Fun(element(I, First))));
+                                      _ when I == L+1 ->
+                                          Acc;
+                                      _ ->
+                                          This(This, I+1, erlang:append_element(Acc, element(I, First)))
+                                  end
+                          end,
+                   Temp(Temp, K, Acc0)
+           end,
+    [Func(1, {}) | keymap(Fun, N, Rest)].
+
+
+keymember(_Key, _N, []) -> false;
+keymember(Key, N, [First|Rest]) when N>=1, is_tuple(First) ->
+    try element(N ,First) of
+        V when V == Key ->
+            true;
+        _ ->
+            keymember(Key, N, Rest)
+    catch
+        error:badarg ->
+            keymember(Key, N, Rest)
+    end.
+
+keymerge(_N, List1, []) -> List1;
+keymerge(_N, [], List2) -> List2;
+keymerge(N, [F1|R1], [F2|R2])
+  when N>=1, is_tuple(F1), is_tuple(F2),
+       element(N, F1) =< element(N, F2) ->
+    [F1 | keymerge(N, R1, [F2|R2])];
+keymerge(N, List1, [F2|R2])
+  when N>=1, is_tuple(F2) ->
+    _ = element(N, F2),
+    [F2 | keymerge(N, List1, R2)].
+
+
+keyreplace(_Key, _N, [], _NewTuple) -> [];
+keyreplace(Key, N, [First|Rest], NewTuple) when N>=1,is_tuple(First) ->
+    try element(N, First) of
+        K when K == Key ->
+            [NewTuple | Rest];
+        _Other ->
+            [First | keyreplace(Key, N, Rest, NewTuple)]
+    catch
+        error:badarg ->
+            [First | keyreplace(Key, N, Rest, NewTuple)]
+    end.
+
+
+keysearch(_Key, _N, []) -> false;
+keysearch(Key, N, [First|Rest]) when N>=1, is_tuple(First) ->
+    try element(N, First) of
+        K when K == Key ->
+            {value, First};
+        _Other ->
+            keysearch(Key, N, Rest)
+    catch
+        error:badarg ->
+            keysearch(Key, N, Rest)
+    end.
+
+
+keysort(_N, []) -> [];
+keysort(N, [First|Rest]) when N>=1, is_tuple(First)->
+    Left = filter(fun(Elem)->element(N, Elem)<element(N, First) end, Rest),
+    Right = filter(fun(Elem)->element(N, Elem)>=element(N, First) end, Rest),
+    append([keysort(N, Left), [First], keysort(N, Right)]).
+
+
+
+keystore(_Key, _N, [], NewTuple) when is_tuple(NewTuple)-> [NewTuple];
+keystore(Key, N, [First|Rest], NewTuple) when N>=1, is_tuple(NewTuple), is_tuple(First) ->
+    try element(N ,First) of
+        V when V == Key ->
+            [NewTuple | Rest];
+        _Other ->
+            [First | keystore(Key, N, Rest, NewTuple)]
+    catch
+        error:badarg ->
+            [First | keystore(Key, N, Rest, NewTuple)]
+    end.
+
+
+keytake(Key, N, List) when N>=1 ->
+    case keyfind(Key, N, List) of
+        false ->
+            false;
+        Tuple when is_tuple(Tuple)->
+            {value, Tuple, keydelete(Key, N, List)};
+        _ ->
+            error(wrong)
+    end.
